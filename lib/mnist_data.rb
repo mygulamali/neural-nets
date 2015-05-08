6d06593a910b938d11ebdcb7ac08800b3e800ca4
Murtaza Gulamali <mygulamali@gmail.com>
2015-05-08 19:06:50 +0100

    Add classes to manage MNIST datasets.

---
+++ lib/mnist_data.rb
@@ -0,0 +1,50 @@
+module MNISTData
+  class DataUnpackingError < RuntimeError
+  end
+
+  class Labels
+    def initialize(filename)
+      datafile = File.binread(filename)
+      @data = datafile.unpack('NNC*')
+      @metadata = @data.shift(2)
+      raise DataUnpackingError if count != @data.length
+    end
+
+    def count
+      @metadata[1]
+    end
+
+    def label(index)
+      @data[index] if index.between?(0, count - 1)
+    end
+  end
+
+  class Images
+    def initialize(filename)
+      datafile = File.binread(filename)
+      @data = datafile.unpack('NNNNC*')
+      @metadata = @data.shift(4)
+      raise DataUnpackingError if count*width*height != @data.length
+    end
+
+    def count
+      @metadata[1]
+    end
+
+    def width
+      @metadata[2]
+    end
+
+    def height
+      @metadata[3]
+    end
+
+    def image(index)
+      if index.between?(0, count - 1)
+        pixels = width*height
+        range = (index*pixels...(index + 1)*pixels)
+        @data[range]
+      end
+    end
+  end
+end
