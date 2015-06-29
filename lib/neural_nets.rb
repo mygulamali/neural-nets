adc941a36822d7650edf4b2248e080080d11184c
Murtaza Gulamali <mygulamali@gmail.com>
2015-06-29 11:44:09 +0100

    Add doc string documentation.

--- lib/neural_nets.rb
+++ lib/neural_nets.rb
@@ -2,5 +2,10 @@ require 'nmatrix'
 require_relative 'neural_nets/math'
 require_relative 'neural_nets/network'
 
+##
+# A module to implement the stochastic gradient descent learning algorithm for a
+# feedforward neural network.  Gradients are calculated using backpropagation.
+# Note that I have focused on making the code simple, easily readable, and
+# easily modifiable.  It is not optimized, and omits many desirable features.
 module NeuralNets
 end
--- lib/neural_nets/math.rb
+++ lib/neural_nets/math.rb
@@ -1,9 +1,13 @@
 module NeuralNets
   class Math
+    ##
+    # The sigmoid function
     def self.sigmoid(x)
       1.0/(1.0 + Math.exp(-x))
     end
 
+    ##
+    # NMatrix version of the sigmoid function
     def self.sigmoid_vec(x)
       one = NMatrix.ones([x.rows, x.cols])
       one/(one + (-x).exp)
