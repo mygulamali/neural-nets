b72b6f346c3c48597616472fd26e0043a3eeea81
Murtaza Gulamali <mygulamali@gmail.com>
2015-07-01 16:48:29 +0100

    Add command to train network.

--- lib/commands.rb
+++ lib/commands.rb
@@ -2,10 +2,13 @@ require 'thor'
 
 require_relative 'image'
 require_relative 'mnist_data'
+require_relative 'neural_nets'
 
 class Commands < Thor
   include Thor::Actions
 
+  MNIST_TRAINING_IMAGE_DATA = 'data/train-images-idx3-ubyte'
+  MNIST_TRAINING_LABEL_DATA = 'data/train-labels-idx1-ubyte'
   MNIST_TEST_IMAGE_DATA = 'data/t10k-images-idx3-ubyte'
   MNIST_TEST_LABEL_DATA = 'data/t10k-labels-idx1-ubyte'
 
@@ -23,5 +26,28 @@ class Commands < Thor
     label_data = MNISTData::Labels.new(MNIST_TEST_LABEL_DATA)
     label = label_data.label(n)
     print "Done. Label is: #{label}\n"
+  end
+
+  desc "train INTERNAL_NODES EPOCHS BATCH_SIZE ETA", "Train the network with the specified parameters."
+  def train(internal_nodes, epochs, batch_size, eta)
+    print "Getting training data... "
+    training_image_data = MNISTData::Images.new(MNIST_TRAINING_IMAGE_DATA)
+    training_label_data = MNISTData::Labels.new(MNIST_TRAINING_LABEL_DATA)
+    print "Done.\n"
+
+    print "Getting test data... "
+    test_image_data = MNISTData::Images.new(MNIST_TEST_IMAGE_DATA)
+    test_label_data = MNISTData::Labels.new(MNIST_TEST_LABEL_DATA)
+    print "Done.\n"
+
+    print "Preparing data for training... "
+    training_data = training_image_data.as_matrices.zip(training_label_data.as_matrices)
+    test_data = test_image_data.as_matrices.zip(test_label_data.as_matrices)
+    print "Done.\n"
+
+    puts "Training..."
+    network = NeuralNets::Network.new([784, internal_nodes.to_i, 10])
+    network.sgd(training_data.first(10000), epochs.to_i, batch_size.to_i, eta.to_f, test_data)
+    puts "Done."
   end
 end
